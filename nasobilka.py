ec184b278b116f61619686e18e9993bdd5699ced
vaclavsv1 <pollsrebsky12@gmail.com>
2024-03-11 09:31:55 +0100

    .

--- nasobilka.py
+++ nasobilka.py
@@ -4,16 +4,25 @@ def nasobeni(x, y):
     vysledek = x * y
     return vysledek
 
+pocet_spravnych_odpovedi = 0
+if uzivatel == spravna:
+        pocet_spravnych_odpovedi += 1
+
 def vyhodnoceni(vysledek, porovnani):
     odpoved = False
     if vysledek == porovnani:
-        print("Máš to správně , jsi šikulka!")
+        print("Máš to správně , jsi šikulka")
+        odpoved = True
     else:
-        print("Ahh , špatně")
+        print("Ahh , máš to špatně")
     return odpoved
 
-for x in range(11):
+for x in range(10):
     x = random.randint(0,10)
     y = random.randint(0,10)
 
-    porovnani = input(f"{x} * {y} = ")
+    porovnani = int(input(f"{x} * {y} = "))
+    vysledek = int(nasobeni(x, y))
+    vyhodnoceni(vysledek, porovnani)
+else:
+    print("Počet správných odpovědí je: {pocet_spravnych_odpovedi}/10")
